86a72ea4e1ac7a1bfdd09ddcac24b3e0fabfb57e
Andrei Markouski <insawm@gmail.com>
2021-11-29 23:22:12 +0300

    few changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,68 @@
-console.log();
-/*  let str = 'asd4567';
-    console.log(str.substr(0,9)); */
-/*     console.log(login.length);
-      console.log(login.substr(0,10)); */
+document.querySelectorAll('input').forEach(function (inputNode) {
+  // focus event
+  inputNode.onfocus = function () {
+    if (this.style.borderColor != 'red') this.style.borderColor = 'aqua';
+  };
 
-document.querySelector('form').addEventListener('change', (event) => {
-  if (event.target.matches('.login')) {
-    let login = event.target.value;
+  // blur event
+  inputNode.onblur = function () {
+    if (this.style.borderColor != 'red') this.style.borderColor = '#E5E5E5';
+  };
 
-    if (login.length > 10) {
-      event.target.value = login.substr(0, 10);
+  // oninput event
+  let errorDisplayDuration = null;
+  inputNode.oninput = function () {
+    // handler for the login field
+    if (this.matches('.login')) {
+      let login = this.value;
 
-      let highlightLogin = setInterval(function () {
-        if (event.target.style.borderColor != 'red') {
-          event.target.style.borderColor = 'red';
-        }else{
-            event.target.style.borderColor = '#E5E5E5';
+      if (login.length > 10) {
+        this.value = login.substr(0, 10);
+
+        let loginError = document.querySelector('.loginError');
+        if (!loginError.hidden) {
+          clearTimeout(errorDisplayDuration);
         }
-      }, 500);
 
-      setTimeout(() => {
-        clearInterval(highlightLogin);
-      }, 5000);
+        loginError.hidden = false;
+        inputNode.style.borderColor = 'red';
+
+        errorDisplayDuration = setTimeout(() => {
+          loginError.hidden = true;
+          inputNode.style.borderColor = 'aqua';
+        }, 2000);
+      }
     }
-  }
-});
 
-document.querySelector('input[type="email"]').addEventListener('focus', (event) => {
-    event.target.style.borderColor= 'aqua';
+    // handler for the password field
+    let passwordError = document.querySelectorAll('.passwordError');
+    if (this.matches('.password')) {
+      let password = document.querySelectorAll('.password');
+      if ((password[0].value != password[1].value) && (password[0].value !== false) && (password[1].value !== false)) {
+        highlightError(password[0]);
+        highlightError(password[1]);
+
+        passwordError[0].hidden = false;
+        passwordError[1].hidden = false;
+      } else {
+        passwordError[0].hidden = true;
+        passwordError[1].hidden = true;
+      }
+    }
+  };
+
+  function highlightError(node) {
+    let highlight = setInterval(function () {
+      if (node.style.borderColor != 'red') {
+        node.style.borderColor = 'red';
+      } else {
+        node.style.borderColor = '#E5E5E5';
+      }
+    }, 300);
+
+    setTimeout(() => {
+      clearInterval(highlight);
+      node.style.borderColor = '#E5E5E5';
+    }, 3100);
+  }
 });
