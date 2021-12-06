9da44eb04e1433a78874f42594060dcc9b630098
Andrei Markouski <insawm@gmail.com>
2021-12-06 13:40:36 +0300

    complete of task form

--- build/js/script.js
+++ build/js/script.js
@@ -1,21 +1,26 @@
-let errFocusNode;
+let onfocusNode;
+let errorDisplayDuration;
 const borderColorOnFocus = 'aqua';
 const borderColorOnBlur = '#E5E5E5';
 const errorColor = 'red';
 let firstEntryPassword = 0;
 const password = document.querySelector('.password');
 const passwordConfirm = document.querySelector('.confirm-password');
-let validatePersonalInfo = { login: false, email: false, password: false };
+let personalInfo = { login: false, email: false, password: false };
 
 document.querySelector('form').onsubmit = function () {
   this.action = '#';
-  if (validateForm(validatePersonalInfo)) console.log(validatePersonalInfo);
+  if (validateForm(personalInfo)) {
+    alert('login: ' + personalInfo.login + ' email: ' + personalInfo.email + ' password: ' + personalInfo.password);
+  } else {
+    alert('Проверьте правильность ввода данных.\n    Поля не должны быть пустыми!\n       Поля не должны содержать ошибки!');
+  }
 };
 
 document.querySelectorAll('input').forEach(function (currentFocusNode) {
-  // focus event
+  // set focus event //
   currentFocusNode.onfocus = function () {
-    errFocusNode = currentFocusNode;
+    onfocusNode = currentFocusNode;
     if (this.style.borderColor != errorColor) this.style.borderColor = borderColorOnFocus;
 
     if (this.matches('.email')) {
@@ -24,19 +29,22 @@ document.querySelectorAll('input').forEach(function (currentFocusNode) {
     }
   };
 
-  // blur event
+  // lost focus event //
   currentFocusNode.onblur = function () {
     if (this.style.borderColor != errorColor) this.style.borderColor = borderColorOnBlur;
 
+    // email field out of focus event handler
     if (this.matches('.email')) {
       if (!validateEmail(this.value)) {
         this.style.borderColor = errorColor;
         mailErrorHighlight(false);
+        personalInfo.email = false;
       } else {
-        validatePersonalInfo.email = this.value;
+        personalInfo.email = this.value;
       }
     }
 
+    // password field out of focus event handler
     if (this.matches('.password')) {
       if (firstEntryPassword) {
         if (firstEntryPassword === 'yes') firstEntryPassword = 'no';
@@ -48,39 +56,19 @@ document.querySelectorAll('input').forEach(function (currentFocusNode) {
     }
   };
 
-  // oninput event
-  let errorDisplayDuration = null;
+  // oninput event //
   currentFocusNode.oninput = function () {
-    // handler for the login field
+    // login field handler on input event
     if (this.matches('.login')) {
       let login = this.value;
-
       if (login.length > 10) {
         this.value = login.substr(0, 10);
-        // validatePersonalInfo.login = login.substr(0, 10);
-
-        let loginError = document.querySelector('.login-error');
-        if (!loginError.hidden) {
-          clearTimeout(errorDisplayDuration);
-        }
-
-        loginError.hidden = false;
-        currentFocusNode.style.borderColor = errorColor;
-
-        errorDisplayDuration = setTimeout(() => {
-          loginError.hidden = true;
-
-          if (errFocusNode === currentFocusNode) {
-            currentFocusNode.style.borderColor = borderColorOnFocus;
-          } else {
-            currentFocusNode.style.borderColor = borderColorOnBlur;
-          }
-        }, 2000);
+        setTimerLoginError();
       }
-      validatePersonalInfo.login = this.value;
+      personalInfo.login = this.value;
     }
 
-    // handler for the password field
+    // password field handler on input event
     if (this.matches('.password')) {
       if (firstEntryPassword === 'no') {
         if (password.value === passwordConfirm.value) {
@@ -92,25 +80,48 @@ document.querySelectorAll('input').forEach(function (currentFocusNode) {
   };
 });
 
-////////////////////// function    /////////////////////
+//////////////////////    function    /////////////////////
+
+function setTimerLoginError() {
+  const loginErrorNode = document.querySelector('.login-error');
+  const loginNode = document.querySelector('.login');
+
+  if (!loginErrorNode.hidden) {
+    clearTimeout(errorDisplayDuration);
+  }
+
+  loginErrorNode.hidden = false;
+  loginNode.style.borderColor = errorColor;
+
+  errorDisplayDuration = setTimeout(() => {
+    loginErrorNode.hidden = true;
+
+    if (onfocusNode === loginNode) {
+      loginNode.style.borderColor = borderColorOnFocus;
+    } else {
+      loginNode.style.borderColor = borderColorOnBlur;
+    }
+  }, 2000);
+}
+
 function mailErrorHighlight(hidden) {
-  let mailError = document.querySelector('.email-error');
-  if (hidden) mailError.hidden = true;
-  else mailError.hidden = false;
+  const mailErrorNode = document.querySelector('.email-error');
+  if (hidden) mailErrorNode.hidden = true;
+  else mailErrorNode.hidden = false;
 }
 
 function equalsPassword(equal) {
-  const passwordError = document.querySelectorAll('.password-error');
+  const passwordsError = document.querySelectorAll('.password-error');
   if (equal) {
-    validatePersonalInfo.password = true;
-    passwordError[0].hidden = true;
-    passwordError[1].hidden = true;
+    personalInfo.password = password.value;
+    passwordsError[0].hidden = true;
+    passwordsError[1].hidden = true;
     password.style.borderColor = borderColorOnBlur;
     passwordConfirm.style.borderColor = borderColorOnBlur;
   } else {
-    validatePersonalInfo.password = false;
-    passwordError[0].hidden = false;
-    passwordError[1].hidden = false;
+    personalInfo.password = false;
+    passwordsError[0].hidden = false;
+    passwordsError[1].hidden = false;
     password.style.borderColor = errorColor;
     passwordConfirm.style.borderColor = errorColor;
   }
@@ -118,15 +129,16 @@ function equalsPassword(equal) {
 
 function validateEmail(email) {
   const mailRegexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const mailRegexpStrong = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  const mailRegexpStrong = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
   const RFC22 = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-  const michaelRushton = /^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/id;
-return mailRegexpStrong.test(email);
+  const michaelRushton = /^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/di;
+
+  return mailRegexpStrong.test(email);
 }
 
-function validateForm(form) {
-  for (let isFieldValid in form) {
-    if (!form[isFieldValid]) {
+function validateForm(formFields) {
+  for (let isFieldValid in formFields) {
+    if (!formFields[isFieldValid]) {
       return false;
     }
   }
